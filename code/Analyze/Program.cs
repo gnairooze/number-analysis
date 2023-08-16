c179b6abf541537b29fe38b3b3a673fe574bdd66
George Nairooze <george.nairooze@linkdev.com>
2023-08-16 12:57:26 +0300

    first commit

---
+++ code/Analyze/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Analyze
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            var resutls = AnalyzeNumber(string.Empty, out string message, out bool isExit);
+
+            if (isExit)
+            {
+                return;
+            }
+        }
+
+        public static List<int>? AnalyzeNumber(string? input, out string message, out bool isExit)
+        {
+            message = string.Empty;
+            isExit = false;
+
+            Console.WriteLine("Write x to exit.");
+            Console.WriteLine("Enter a number greater than 0 to analyze:");
+            input = Console.ReadLine();
+
+            if (input == "x")
+            {
+                isExit = true;
+                Console.WriteLine("exit analysis.");
+                return null; 
+            }
+            
+            bool parseSucceeded = int.TryParse(input, out int number);
+
+            if (!parseSucceeded)
+            {
+                Console.WriteLine("Invalid number");
+
+                return AnalyzeNumber(input, out message, out isExit);
+            }
+
+            var results = NumberAnalysis.Business.Manager.AnalyzeNumber(number, out message);
+
+            if (!string.IsNullOrEmpty(message))
+            {
+                Console.WriteLine(message);
+
+                return AnalyzeNumber(input, out message, out isExit);
+            }
+
+            if (results == null)
+            {
+                Console.WriteLine("no analysis for this number");
+                return AnalyzeNumber(input, out message, out isExit);
+            }
+
+            Console.WriteLine("resuts:");
+            foreach (var item in results)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("------");
+
+            return AnalyzeNumber(input, out message, out isExit);
+        }
+    }
+}
+
---
+++ code/NumberAnalysis.Business/Manager.cs
@@ -0,0 +1,43 @@
+﻿namespace NumberAnalysis.Business
+{
+    public static class Manager
+    {
+        public static List<int>? AnalyzeNumber(int number, out string message)
+        {
+            message = string.Empty;
+
+            #region validations
+            if (number < 0)
+            {
+                message = "Number must be greater than 0";
+                return null;
+            }
+            if(number == 0)
+            {
+                message = "Number must be greater than 0";
+                return null;
+            }
+            #endregion
+
+            List<int>? numbers = new();
+
+            if (number == 1)
+            {
+                numbers.Add(1);
+                return numbers;
+            }
+            
+            for (int i = 2; i <= number; i++)
+            {
+                if(number % i == 0)
+                {
+                    numbers.Add(i);
+                    number = number / i;
+                    i = 1;
+                }
+            }
+            
+            return numbers;
+        }   
+    }
+}
